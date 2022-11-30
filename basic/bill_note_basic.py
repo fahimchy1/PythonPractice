bdf54912ea4f6ffcb3e68d0bfabedd818eab6cac
Fahim Chowdhury <118947490+fahimchy1@users.noreply.github.com>
2022-11-30 12:10:32 +0600

    Add files via upload

---
+++ basic/bill_note_basic.py
@@ -0,0 +1,43 @@
+
+# bill = int(input('please enter your bill:- '))
+# a=0
+# a=bill//1000
+# print('1000 taka note needed:-', a)
+# bill = bill%1000
+#
+# a=bill//500
+# print('500 taka note needed:-', a)
+# bill = bill-(a*500)
+#
+# a=bill//200
+# print('200 taka note needed:-', a)
+# bill = bill-(a*200)
+#
+# a=bill//100
+# print('100 taka note needed:-', a)
+# bill = bill-(a*100)
+#
+# a=bill//50
+# print('50 taka note needed:-', a)
+# bill = bill-(a*50)
+#
+# a=bill//20
+# print('20 taka note needed:-', a)
+# bill = bill-(a*20)
+#
+# a=bill//10
+# print('10 taka note needed:-', a)
+# bill = bill-(a*10)
+#
+# a=bill//5
+# print('5 taka note needed:-', a)
+# bill = bill-(a*5)
+#
+# a=bill//2
+# print('2 taka note needed:-', a)
+# bill = bill-(a*2)
+#
+#
+# a=bill//1
+# print('1 taka needed:-', a)
+
