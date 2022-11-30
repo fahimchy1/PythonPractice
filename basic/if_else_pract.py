aaa4256838280c390cf61a5d6e9b158c02f6527b
Fahim Chowdhury <118947490+fahimchy1@users.noreply.github.com>
2022-11-30 12:12:08 +0600

    Add files via upload

---
+++ basic/if_else_pract.py
@@ -0,0 +1,45 @@
+#No:1
+# x = int(input())
+#
+# if x%3==0 and x%5==0:
+#     print("Yes")
+# else:
+#     print("No")
+
+#No:2
+# x=int(input())
+#
+# if x>0:
+#     print('positive')
+# elif x<0:
+#     print('Negative')
+# else:
+#     print('Zero')
+
+#N0:3
+# x=int(input())
+#
+# if x%2==0:
+#     print('even')
+# else:
+#     print('odd')
+
+# No:4
+# x = str(input())
+#
+# if x >= 'a' and x <='z':
+#     print('Lowercase')
+# elif x>='A' and x<= 'Z':
+#     print('Uppercase')
+# else:
+#     print('Nothing')
+
+# No:5
+# x = str(input())
+# if x>='a' and x<='z' or x >= 'A' and x <='Z':
+#     if x in 'aeiouAEIOU':
+#         print('Vowel')
+#     else:
+#         print('Consonent')
+
+# No:6
