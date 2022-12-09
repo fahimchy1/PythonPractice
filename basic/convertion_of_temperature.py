00b65a030e397a66488fcbbb5c2f8527ec13c9d7
Fahim Chowdhury <118947490+fahimchy1@users.noreply.github.com>
2022-12-09 23:55:01 +0600

    Add files via upload

---
+++ basic/convertion_of_temperature.py
@@ -0,0 +1,8 @@
+def fahrenheit_to_celsius(temp):
+    return (temp-32) * 5/9
+# val = fahrenheit_to_celsius(765)
+# print(val)
+print(fahrenheit_to_celsius(100))
+def celsius_to_fahrenheit(temp):
+    return (temp*9/5)+32
+print(celsius_to_fahrenheit(-40))
