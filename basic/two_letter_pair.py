ca0d63e372c623f78e4d000ecd210e7d20986e50
Fahim Chowdhury <118947490+fahimchy1@users.noreply.github.com>
2022-12-10 23:26:04 +0600

    Add files via upload

---
+++ basic/two_letter_pair.py
@@ -0,0 +1,9 @@
+for i in range(65,90+1):
+    for j in range(65,90+1):
+        print(chr(i)+chr(j))
+
+#================
+for i in range(65,90+1):
+    for j in range(65,90+1):
+        if i!=j:
+            print(chr(i)+chr(j))
